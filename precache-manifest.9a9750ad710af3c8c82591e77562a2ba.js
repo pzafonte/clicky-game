35db7776a750e453cab8461b9c957a5e7fa6b767
Peter David Zafonte <pzafonte@users.noreply.github.com>
2018-11-13 22:54:49 -0500

    Updates

---
+++ precache-manifest.9a9750ad710af3c8c82591e77562a2ba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6acb6692e4ee25941da6",
+    "url": "/clicky-game/static/js/runtime~main.6acb6692.js"
+  },
+  {
+    "revision": "99bb4947e88e6399476b",
+    "url": "/clicky-game/static/js/main.99bb4947.chunk.js"
+  },
+  {
+    "revision": "9f39fde1f1a76620d1b3",
+    "url": "/clicky-game/static/js/1.9f39fde1.chunk.js"
+  },
+  {
+    "revision": "99bb4947e88e6399476b",
+    "url": "/clicky-game/static/css/main.c0096e8d.chunk.css"
+  },
+  {
+    "revision": "5418b76c67b976f5bc17079fd8a96d8b",
+    "url": "/clicky-game/index.html"
+  }
+];
